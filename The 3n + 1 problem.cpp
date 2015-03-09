f08ffe6bf02c8dc47ca5701005548566efc886d5
xanarry <xiong-yang@qq.com>
2015-03-09 19:14:25 +0800

    Update The 3n + 1 problem.cpp

--- The 3n + 1 problem.cpp
+++ The 3n + 1 problem.cpp
@@ -19,39 +19,33 @@ Sample Output
 201 210 89
 900 1000 174
 */
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	int i, j;
-	while (cin >> i >> j)
-	{
-		long long max_l = 0;
-		for (int n = min(i, j); n <= max(i, j); n++)
-		{
-			long long t = n;
-			long long times = 1;
-			while (t != 1)
-			{
-				times++;
-				if (t % 2)
-					t = 3 * t + 1;
-				else
-					t = t / 2;
-			}
-			if (times > max_l)
-				max_l = times;
-		}
-		cout << i << " " << j << " " << max_l << endl;
-	}
-	return 0;
+    int i, j;
+    while (cin >> i >> j)
+    {
+        long long max_l = 0;
+        for (int n = min(i, j); n <= max(i, j); n++)
+        {
+            long long t = n;
+            long long times = 1;
+            while (t != 1)
+            {
+                times++;
+                if (t % 2)
+                t = 3 * t + 1;
+                else
+                t = t / 2;
+            }
+            if (times > max_l)
+            max_l = times;
+        }
+        cout << i << " " << j << " " << max_l << endl;
+    }
+    return 0;
 }
-
-/*
-1 10 20
-100 200 125
-201 210 89
-900 1000 174
-*/
